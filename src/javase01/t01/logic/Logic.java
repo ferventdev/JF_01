f70b9c09352116c06d2eba4aead31339700b3ddf
ferventdev <alex.4everyours@gmail.com>
2017-06-02 11:53:29 +0300

    Initial commit

---
+++ src/javase01/t01/logic/Logic.java
@@ -0,0 +1,10 @@
+package javase01.t01.logic;
+
+/**
+ * Created by Aleksandr Shevkunenko on 02.06.2017.
+ */
+public class Logic {
+    public String method() {
+        return "I am string in Logic";
+    }
+}
