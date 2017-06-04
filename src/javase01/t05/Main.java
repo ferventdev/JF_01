d6bfce310d3cb9aca1ac793df84cfbd1808aca54
ferventdev <alex.4everyours@gmail.com>
2017-06-04 15:43:33 +0300

    Task 5 finished

--- src/javase01/t05/Main.java
+++ src/javase01/t05/Main.java
@@ -4,7 +4,17 @@ package javase01.t05;
  * Created by Aleksandr Shevkunenko on 04.06.2017.
  */
 public class Main {
-    public static void main(String[] args) {
+    private static final int MATRIX_SIZE = 7;   // эту константу можно изменить
 
+    public static void main(String[] args) {
+        int[][] matrix = new int[MATRIX_SIZE][MATRIX_SIZE];
+        System.out.printf("Полученная матрица размером %dx%d:%n", MATRIX_SIZE, MATRIX_SIZE);
+        for(int row = 0; row < MATRIX_SIZE; row++) {
+            for(int col = 0; col < MATRIX_SIZE; col++) {
+                matrix[row][col] = ((row == col) || (row == MATRIX_SIZE - col - 1)) ? 1 : 0;
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
     }
 }
