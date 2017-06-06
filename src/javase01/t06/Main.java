41d9a656b86296b8fb6dd7199d3bc47f15f251dd
ferventdev <alex.4everyours@gmail.com>
2017-06-06 15:00:13 +0300

    Task 6 - testing started

--- src/javase01/t06/Main.java
+++ src/javase01/t06/Main.java
@@ -5,6 +5,45 @@ package javase01.t06;
  */
 public class Main {
     public static void main(String[] args) {
+        Notepad pad = new Notepad(4);
+        System.out.println("The notepad was just created.");
+        pad.watchAllRecords();
+        System.out.printf("Capacity = %d, number of records = %d.%n%n", pad.currentCapacity(), pad.getNumberOfRecords());
 
+        System.out.println("Let's add some records to the notepad.");
+        for (int i = 1; i <= 11; i++) System.out.print(pad.addRecord(new Record("Rec #" + i + ". ")) + " ");
+        System.out.print(pad.addRecord(null) + " ");    // check adding of a null value
+        System.out.println();
+        pad.watchAllRecords();
+        System.out.printf("Capacity = %d, number of records = %d.%n%n", pad.currentCapacity(), pad.getNumberOfRecords());
+
+        System.out.println("Let's edit the existing records in the notepad.");
+        try {
+            pad.editRecord(pad.getNumberOfRecords(), new Record("This edit won't be allowed."));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            pad.editRecord(pad.getNumberOfRecords() - 1, null);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        for (int i = 1, n = pad.getNumberOfRecords(); i <= n - 2; i++)
+            System.out.print(pad.editRecord(i - 1, new Record("New rec #" + i + ". ")) + " ");
+        System.out.println();
+        pad.watchAllRecords();
+        System.out.printf("Capacity = %d, number of records = %d.%n%n", pad.currentCapacity(), pad.getNumberOfRecords());
+
+        System.out.println("Let's remove records from the notepad.");
+        try {
+            pad.removeRecord(pad.getNumberOfRecords()); // this won't be allowed
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+        for (int i = 0, n = pad.getNumberOfRecords(); i < n; i++)
+            System.out.print(pad.removeRecord() + " ");
+        System.out.println();
+        pad.watchAllRecords();
+        System.out.printf("Capacity = %d, number of records = %d.%n%n", pad.currentCapacity(), pad.getNumberOfRecords());
     }
 }
--- src/javase01/t06/Notepad.java
+++ src/javase01/t06/Notepad.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * Created by Aleksandr Shevkunenko on 04.06.2017.
  */
 public class Notepad {
-    private static final int DEFAULT_CAPACITY = 2;
+    private static final int DEFAULT_CAPACITY = 1;
     private Record[] records;
     private int numberOfRecords = 0;
 
@@ -25,7 +25,8 @@ public class Notepad {
     /** Prints all records in the notepad to the standard output. */
     public void watchAllRecords() {
         System.out.println("All records in the notepad:");
-        for (Record rec : records) System.out.println(rec);
+        for (Record rec : records) System.out.print(rec + "  ");
+        System.out.println();
     }
 
     /** Returns the current capacity of the notepad
@@ -60,17 +61,20 @@ public class Notepad {
             throw new IndexOutOfBoundsException("Record index cannot exceed the number of records.");
         Record removedRecord = records[recordIndex];
         numberOfRecords--;
-        if (numberOfRecords <= records.length / 4) {    // we'll reallocate data to a shorter array
-            Record[] destination = new Record[numberOfRecords];
+        if (DEFAULT_CAPACITY <= numberOfRecords && numberOfRecords <= records.length / 4) {
+            // we'll reallocate data to a shorter array
+            System.out.print("***" + recordIndex + " ");
+            Record[] destination = new Record[numberOfRecords * 2];
             System.arraycopy(records, 0, destination, 0, recordIndex);
-            System.arraycopy(records, recordIndex + 1, destination, recordIndex, numberOfRecords - recordIndex - 1);
+            System.arraycopy(records, recordIndex + 1, destination, recordIndex, numberOfRecords - recordIndex);
             records = destination;
         } else if (recordIndex == numberOfRecords) {    // no reallocation; the last record was removed
+            System.out.print("**");
             records[numberOfRecords] = null;
-        }
-        else {
+        } else {
             // no reallocation, but we need to "put together" two parts of our array because of the removed record "in the middle"
-            System.arraycopy(records, recordIndex + 1, records, recordIndex, numberOfRecords - recordIndex - 1);
+            System.out.print("*");
+            System.arraycopy(records, recordIndex + 1, records, recordIndex, numberOfRecords - recordIndex);
             records[numberOfRecords] = null;
         }
         return removedRecord;
